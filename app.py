6d8d40da89880e98a5b5c42c5b9bf3f620d0d953
kirtika01 <rutvikkirtika@gmail.com>
2024-02-20 20:59:52 +0530

    first

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # Q&A Chatbot
-from langchain.llms import OpenAI
+# from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
+
+# from langchain.llms import OpenAI
 import streamlit as st
 from dotenv import load_dotenv
 import os
@@ -8,23 +11,24 @@ load_dotenv()
 
 
 def get_openai_response(question):
-    llm = OpenAI(
+    llm = ChatOpenAI(
         openai_api_key=os.getenv("OPEN_API_KEY"),
-        model_name="text-davinci-003",
+        model_name="gpt-3.5-turbo",
         temperature=0.5,
     )
-    response = llm(question)
+    response = llm.invoke(question)  # Updated function call
     return response
 
-    st.set_page_config(page_title="Q&A Demo")
 
-    st.header("Langchain Application")
+st.set_page_config(page_title="Q&A Demo")
 
-    input = st.text_input("Input: ", key="input")
-    response = get_openai_response(input)
+st.header("Langchain Application")
 
-    submit = st.button("Ask the question")
+input = st.text_input("Input: ", key="input")
+response = get_openai_response(input)
 
-    if submit:
-        st.subheader("The Response is:")
-        st.write(response)
+submit = st.button("Ask the question")
+
+if submit:
+    st.subheader("The Response is:")
+    st.write(response)
